d1e26b3d8ff18be7eb63d9f3c59bd5a634d8b97c
aybüke <aybukeegrbzz@gmail.com>
2023-03-11 12:42:03 +0300

    11.03.2023

---
+++ odev1.py
@@ -0,0 +1,24 @@
+#***Soru 1***
+#Python'da Veri Tipleri
+#String (metin) veri tipleri → str
+#Numerik (sayısal) veri tipleri → int, float, complex
+#Sequence (sıralama) veri tipleri → list, tuple, range
+#Mapping (haritalama) veri tipleri → dict
+#Set veri tipleri → set, frozenset
+#Boolean veri tipleri → bool
+#Binary veri tipleri → bytes, bytearray, memoryview
+
+#***Soru 2***
+#Kodlamaio anasayfasında yer alan  Kurslarım, Tüm Kurslar, Kariyer, Sık Sorulan Sorular 
+  #gibi metinsel ifadeler String Veri tipine örnektir.
+#Kodlamaio anasayfasında bulunan Kategori, Eğitmen list veri türüne örnektir.
+#Kodlamaio sitesinde kurs tamamlama yüzdesi ise int veri tipine örnektir.
+#Kodlamaio sitesinde siteye giriş yapılıp yapılmadığının kontrolü ise bool veri tipine örnektir.
+
+#***Soru 3***
+kullanıcıAdi = "Aybüke"
+sifre = "12345"
+if kullanıcıAdi == "Aybüke" and sifre == "12345":
+    print("Gİriş başarılı")
+else:
+    print("Kullanıcı adı veya şifre hatalı")
